70dd06ef9184dcb5c7d426a3433e74770ef52121
Pallavi1162 <avanipallavi3@gmail.com>
2025-07-23 21:02:38 +0530

    Update and rename user input to number to user input to number.py

---
+++ user input to number.py
@@ -0,0 +1,14 @@
+int_text = input("Give me an integer number: ")
+int_num = int(int_text)
+float_text = input("Give me an float number: ")
+float_num = float(float_text)
+result = int_text * float_text
+print("Your result is :", result)
+
+
+shorcut:
+
+int_text = input("Give me an integer number: ")
+float_text = input("Give me an float number: ")
+result = int_text * float_text
+print("Your result is :", result)
