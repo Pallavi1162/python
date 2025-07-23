1e036f67fddf6e52ed22f2f243d572b8726cf2b9
Pallavi1162 <avanipallavi3@gmail.com>
2025-07-23 21:14:32 +0530

    Create math power.py

---
+++ math power.py
@@ -0,0 +1,12 @@
+base_num = int(input("Give me the base number : "))
+power_num = int(input("Give me the power number : "))
+result = base_num ** power_num
+print("Your result is : ", result)
+
+
+Alternative solution:
+
+base_num = int(input("Give me the base number : "))
+power_num = int(input("Give me the power number : "))
+result = pow(base_num , power_num)
+print("Your result is : ", result)
