d16dd627d9904824449eda2ec1b4d2bf9acf03c6
Pallavi1162 <avanipallavi3@gmail.com>
2025-07-24 19:50:08 +0530

    Update user input to number.py

--- user input to number.py
+++ user input to number.py
@@ -7,7 +7,7 @@ print("Your result is :", result)
 
 
 shorcut:
-
+------------------------------------------------
 int_text = input("Give me an integer number: ")
 float_text = input("Give me an float number: ")
 result = int_text * float_text
