2ddcbcbf711c0a484cf9dac73fc01a78adb80106
Pallavi1162 <avanipallavi3@gmail.com>
2025-07-24 19:51:20 +0530

    Update math power.py

--- math power.py
+++ math power.py
@@ -5,7 +5,7 @@ print("Your result is : ", result)
 
 
 Alternative solution:
-
+-------------------------------------------------------
 base_num = int(input("Give me the base number : "))
 power_num = int(input("Give me the power number : "))
 result = pow(base_num , power_num)
